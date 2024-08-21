82bd09ebe1a9fd12d248129b7d0d184cecb97eb1
Yaroslav-Vasin <yaroslav.vasin@bystrov.agency>
2024-08-21 17:05:50 +0300

    counter

---
+++ app.js
@@ -0,0 +1,14 @@
+const App = {
+  data() {
+    return {
+      counter: 0,
+      title: 'Counter',
+    }
+  }
+}
+
+const app = Vue.createApp(App)
+
+app.mount('#app')
+
+// Vue.createApp(App).mount('#app')
