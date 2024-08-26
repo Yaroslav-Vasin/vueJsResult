4618d84fe4be7f66edac626976ed678fef54815f
Yaroslav-Vasin <yaroslav.vasin@bystrov.agency>
2024-08-26 17:40:46 +0300

    first

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: 'Counter',
+      title: 'List of notes',
+      placeholderString: 'Enter the note title',
+      inputValue: '',
+      notes: ['Note 1', 'Note 2']
     }
-  }
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value
+    },
+    addNewNote() {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    toUpperCase(item) {
+      return item.toUpperCase()
+    },
+    removeNote(idx, event) {
+      console.log(event, 'deleted');
+      
+      this.notes.splice(idx, 1)
+    }
+  },
+  computed: {
+    doubleCountComputed() {
+      console.log('doubleCountComputed');
+      
+      return this.notes.length * 2
+    }
+  },
 }
 
 const app = Vue.createApp(App)
