7a67c9a523ddea928bcd4bf7c9a3dac3386591f5
Yaroslav-Vasin <yaroslav.vasin@bystrov.agency>
2024-08-30 16:01:39 +0300

    html 2

--- app.js
+++ app.js
@@ -7,36 +7,40 @@ const app = Vue.createApp({
       count: 0,
       title: 'Hello Vue 3',
       steps: [
-        { id: 1, title: 'Step 1', data: 'Data 1' },
-        { id: 2, title: 'Step 2', data: 'Data 2' },
-        { id: 3, title: 'Step 3', data: 'Data 3' },
-        { id: 4, title: 'Step 4', data: 'Data 4' },
-        { id: 5, title: 'Step 5', data: 'Data 5' }
+        { id: 1, title: 'Core', data: 'Data 1', active: true },
+        { id: 2, title: 'Components', data: 'Data 2', active: true },
+        { id: 3, title: 'Router', data: 'Data 3', active: true },
+        { id: 4, title: 'Vuex', data: 'Data 4', active: true },
+        { id: 5, title: 'Composition', data: 'Data 5', active: false }
       ]
     }
   },
   methods: {
     changeTitle() {
       this.title = 'Changed title!'
+    },
+    isActive(step) {
+      if (step.active) {
+        return 'active'
+      }
     }
   },
   template: `
     <div class="card center">
-      <h1>{{ title }}</h1>
-      <div class="content-wrapper">
-        content 1
-      </div>
-      <div class="label-wrapper">
-        <ul>
-          <li v-for="step in steps">
-            <input type="checkbox" value="{{ step.title }}"></input>
-          </li>
-        </ul>
-      </div>
-      <div class="Buttons-wrapper">
-        <button @click="count++">Back</button>
-        <button @click="changeTitle()">Forward</button>
-      </div>
+        <h1>План по изучению Vue.js</h1>
+        <p>Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API...</p>
+        
+        <nav>
+            <a v-for="(step, index) in steps" href="#step-1" class="isActive(step)" onclick="navigateTo(1)" id="nav-step-1">
+                <div class="step-number">1</div>
+                <div class="step-label">Основы</div>
+            </a>
+        </nav>
+        
+        <div class="buttons">
+            <button id="back-btn" class="button" onclick="prevStep()" disabled>НАЗАД</button>
+            <button id="next-btn" class="button" onclick="nextStep()">ДАЛЕЕ</button>
+        </div>
     </div>
   `,
   beforeCreate() {
