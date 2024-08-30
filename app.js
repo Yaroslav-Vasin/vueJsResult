bd17a97c7e55c4d215ddd71be9b03b7833b8a2df
Yaroslav-Vasin <yaroslav.vasin@bystrov.agency>
2024-08-30 15:35:11 +0300

    html 2

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = Vue.createApp({
   data() {
     return {
       count: 0,
-      title: 'Hello Vue 3'
+      title: 'Hello Vue 3',
+      steps: [
+        { id: 1, title: 'Step 1', data: 'Data 1' },
+        { id: 2, title: 'Step 2', data: 'Data 2' },
+        { id: 3, title: 'Step 3', data: 'Data 3' },
+        { id: 4, title: 'Step 4', data: 'Data 4' },
+        { id: 5, title: 'Step 5', data: 'Data 5' }
+      ]
     }
   },
   methods: {
@@ -21,9 +28,8 @@ const app = Vue.createApp({
       </div>
       <div class="label-wrapper">
         <ul>
-          <li v-for>
-            <span>Count: </span>
-            {{ count }}
+          <li v-for="step in steps">
+            <input type="checkbox" value="{{ step.title }}"></input>
           </li>
         </ul>
       </div>
